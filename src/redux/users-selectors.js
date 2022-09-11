d6f65db35e8af0e7ebf064b1d2eec05324531ead
nikitsch <nikitsch5@gmail.com>
2022-09-11 22:09:21 +0300

    feat: add createSelector

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,20 @@
-export const getUsers = (state) => {
+import { createSelector } from 'reselect';
+
+export const getUsersSelector = (state) => {
   return state.usersPage.users;
 }
+
+export const getUsers = createSelector(getUsersSelector, (users) => {
+  return users.filter(u => true);
+})
+
+// export const getUsers = (state) => {
+//   return state.usersPage.users;
+// }
+
+// export const getUsersSuperSelector = createSelector(getUsers, getIsFetching, (users, isFetching) => {
+//   return users.filter(u => true);
+// })
 
 export const getPageSize = (state) => {
   return state.usersPage.pageSize;
